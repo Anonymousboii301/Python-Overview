e6b151f0fdbb85b01ca24b4dbc0e6f99cefbb22b
Ayush Kumar Singh <182224496+Anonymousboii301@users.noreply.github.com>
2025-09-25 16:42:00 +0530

    Delete Chapter_04 Practice set Loops in Python directory

--- Chapter_04 Practice set Loops in Python/Problem_010.py
+++
@@ -1,9 +0,0 @@
-# 10. Write a program to print multiplication table of n using for loops in reversed 
-# order.
-
-n = int(input("Enter the number please : "))
-
-for i in range(1 , 11):
-    print(f"{n} X {11-i} = {n*(11-i)}")
-    
-    
--- Chapter_04 Practice set Loops in Python/Problem_03.py
+++
@@ -1,8 +0,0 @@
-# 3. Attempt problem 1 using while loop. 
-67
-i = 1
-n = int(input("Enter the number : "))
- 
-while(i<=10):
-    print(f"{n} X {i} = {n*i}")
-    i+=1
