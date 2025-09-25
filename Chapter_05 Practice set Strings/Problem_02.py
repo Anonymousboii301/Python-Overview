d5d2732b2c2cea5fe634035ce87ae220728093c9
Ayush Kumar Singh <182224496+Anonymousboii301@users.noreply.github.com>
2025-09-25 16:51:05 +0530

    Delete Chapter_05 Practice set Strings directory

--- Chapter_05 Practice set Strings/Problem_02.py
+++
@@ -1,7 +0,0 @@
-# Write a program to fill in a letter template given below with name and date. 
-letter = '''  
-Dear <|Name|>, 
-You are selected! 
-<|Date|> 
-'''
-print(letter.replace("<|Name|>" , "Batman").replace("<|Date|>" , "19/11/2023"))
--- Chapter_05 Practice set Strings/Problem_03.py
+++
@@ -1,4 +0,0 @@
-# 3. Write a program to detect double space in a string.
-
-name = "Ayush is a  good boi."
-print(name.find("  "))
