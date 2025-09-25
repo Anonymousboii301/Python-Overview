d704bc427ed7f1ea917abc245aa5512fa7fe12df
Ayush Kumar Singh <182224496+Anonymousboii301@users.noreply.github.com>
2025-09-25 16:39:02 +0530

    Delete Chapter_01 Practice set Intro directory

--- Chapter_01 Practice set Intro/Problem_03.py
+++
@@ -1,9 +0,0 @@
-
-
-# https://pypi.org/project/pyttsx3/
-#  Module for text to speech in python 
-
-import pyttsx3
-engine = pyttsx3.init()
-engine.say("I will speak this text...................................")
-engine.runAndWait()
