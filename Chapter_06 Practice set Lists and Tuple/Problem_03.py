a1467fdc3e3b8fda1e4849343234521832453147
Ayush Kumar Singh <182224496+Anonymousboii301@users.noreply.github.com>
2025-09-25 16:51:41 +0530

    Delete Chapter_06 Practice set Lists and Tuple directory

--- Chapter_06 Practice set Lists and Tuple/Problem_03.py
+++
@@ -1,5 +0,0 @@
-# 3. Check that a tuple type cannot be changed in python.
-tuple1 = (1,2,3,4,5,6,True,"Batman")
-# tuple1[2] = "Ayush"   Error 
-
-print(tuple1)
--- Chapter_06 Practice set Lists and Tuple/Problem_04.py
+++
@@ -1,5 +0,0 @@
-# 4. Write a program to sum a list with 4 numbers. 
-
-mylist = [34,56,78,89]
-
-print(sum(mylist))
